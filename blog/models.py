b7aa738975eec179de777eba15686f3e7f18c116
Yeonguk Kim <rudghk1437@naver.com>
2022-12-12 18:28:20 +0900

    목록 페이지 구현, 템플릿 적용하기

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) # add 할 때의 시간
     updated_at = models.DateTimeField(auto_now=True)     # update 할 때의 사간
     
+    # 모델에 정의돼있는 함순데, 오버라이딩한 것.
     def get_absolute_url(self):
       return f'/blog/{self.pk}/'
 
-def __str__(self):
-    return f'[{self.pk}--{self.title}]'
+    def __str__(self):
+        return f'[{self.pk}--{self.title}]'
